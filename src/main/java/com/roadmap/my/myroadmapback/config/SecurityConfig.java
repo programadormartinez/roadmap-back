4d987110bc3a1d072dc5a20060afafbd2f760dd2
programadormartinez <programadormartinez@gmail.com>
2026-01-02 10:43:18 -0300

    Add config

--- src/main/java/com/roadmap/my/myroadmapback/config/SecurityConfig.java
+++ src/main/java/com/roadmap/my/myroadmapback/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://roadmap-front.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
